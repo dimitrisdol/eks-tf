fdaade7037d96443738fe199718886de60d66c3a
dimitrisdol <jimmdologlou@gmail.com>
2023-05-10 11:02:44 +0300

    EKS cluster setup

---
+++ terraform.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.66.1"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "~> 3.5.1"
+    }
+    tls = {
+      source = "hashicorp/tls"
+      version = "~> 4.0.4"
+    }
+    cloudinit = {
+      source = "hashicorp/cloudinit"
+      version = "~> 2.3.2"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "~> 2.20.0"
+    }
+  }
+  required_version = "~> 1.4.6"
+}
